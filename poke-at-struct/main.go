4372cf6cd04bdedbc5ef0688eca5392b46b3754e
PVillela <pvillela@gmail.com>
2021-04-10 22:40:01 -0400

    Update copyright.

--- poke-at-struct/main.go
+++ poke-at-struct/main.go
@@ -1,3 +1,9 @@
+/*
+ *  Copyright © 2021 Paulo Villela. All rights reserved.
+ *  Use of this source code is governed by the Apache 2.0 license
+ *  that can be found in the LICENSE file.
+ */
+
 package main
 
 import "fmt"
@@ -5,6 +11,32 @@ import "fmt"
 type Foo struct {
 	x int
 	y int
+}
+
+type Bar struct {
+	x int
+	y int
+}
+
+type IY interface {
+	Py() *int
+	Y() int
+}
+
+func (s Foo) Py() *int {
+	return &s.y
+}
+
+func (s Foo) Y() int {
+	return s.y
+}
+
+func (s *Bar) Py() *int {
+	return &s.y
+}
+
+func (s *Bar) Y() int {
+	return s.y
 }
 
 func main() {
@@ -27,4 +59,24 @@ func main() {
 
 	*p = 42
 	fmt.Println("foo1", foo1)
+
+	// Struct poking doesn't work when the struct is passed as an interface
+	foo := Foo{}
+	var ifoo IY = foo
+	*ifoo.Py() = 42
+	fmt.Println("foo", foo)
+	fmt.Println("ifoo", ifoo)
+	fmt.Println("ifoo.Y()", ifoo.Y())
+	fmt.Println("ifoo.Py()", ifoo.Py())
+	fmt.Println("foo.Py() ", foo.Py())
+
+	// Struct poking works when a pointer to the struct is passed as an interface
+	bar := Bar{}
+	var ibar IY = &bar
+	*ibar.Py() = 42
+	fmt.Println("bar", bar)
+	fmt.Println("ibar", ibar)
+	fmt.Println("ibar.Y()", ibar.Y())
+	fmt.Println("ibar.Py()", ibar.Py())
+	fmt.Println("bar.Py() ", bar.Py())
 }
