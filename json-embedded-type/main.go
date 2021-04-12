15cee920e1a858a1dd74b176d2457a8ac67f4672
PVillela <pvillela@gmail.com>
2021-04-12 12:28:10 -0400

    Added json-embedded-type.

---
+++ json-embedded-type/main.go
@@ -0,0 +1,161 @@
+/*
+ *  Copyright © 2021 Paulo Villela. All rights reserved.
+ *  Use of this source code is governed by the Apache 2.0 license
+ *  that can be found in the LICENSE file.
+ */
+
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Foo struct {
+	X1   int    `json:"x1"`
+	X2   int    `json:"x2Foo"`
+	YFoo string `json:"yFoo"`
+}
+
+type Bar struct {
+	X1 int `json:"x1"`
+	X2 int `json:"x2Bar"`
+	Foo
+}
+
+type Baz struct {
+	Foo
+	X1 int `json:"x1"`
+	X2 int `json:"x2Baz"`
+}
+
+type Xyz interface {
+	M1() int
+}
+
+type Fuz struct {
+	Xyz
+	X1 int `json:"x1"`
+	X2 int `json:"x2Fuz"`
+}
+
+func (s Foo) M1() int { return s.X1 }
+
+func (s Bar) M1() int { return s.X1 }
+
+// FuzUnmarshal -- see https://endophage.com/post/golang-parse-to-interface/
+// This is a simple example with poor error handling.
+func FuzUnmarshal(data []byte, fuz *Fuz) {
+	_ = json.Unmarshal(data, fuz)
+	m := make(map[string]json.RawMessage)
+	_ = json.Unmarshal(data, &m)
+	fmt.Println(m["Xyz"])
+	xyzSer, ok := m["Xyz"]
+	if !ok {
+		return
+	}
+	_ = json.Unmarshal(xyzSer, &m)
+	_, ok = m["x2Bar"]
+	if ok {
+		bar := Bar{}
+		_ = json.Unmarshal(xyzSer, &bar)
+		fuz.Xyz = bar
+	} else {
+		foo := Foo{}
+		_ = json.Unmarshal(xyzSer, &foo)
+		fuz.Xyz = foo
+	}
+}
+
+func SerializeAndPrint(label string, object interface{}) []byte {
+	serialized, _ := json.Marshal(object)
+	fmt.Printf("%s as JSON:\n  %s\n", label, serialized)
+	fmt.Printf("%s as string:\n  %v\n", label, object)
+	return serialized
+}
+
+func main() {
+	foo := Foo{
+		X1:   1,
+		X2:   2,
+		YFoo: "y1",
+	}
+
+	func() {
+		fooSer := SerializeAndPrint("foo", foo)
+		fooDes := Bar{}
+		_ = json.Unmarshal(fooSer, &fooDes)
+		SerializeAndPrint("fooDes", fooDes)
+		fmt.Println()
+	}()
+
+	bar := Bar{
+		X1:  100,
+		X2:  200,
+		Foo: foo,
+	}
+
+	func() {
+		barSer := SerializeAndPrint("bar", bar)
+		barDes := Bar{}
+		_ = json.Unmarshal(barSer, &barDes)
+		SerializeAndPrint("barDes", barDes)
+		fmt.Println()
+	}()
+
+	func() {
+		baz := Baz{
+			X1:  91,
+			X2:  92,
+			Foo: foo,
+		}
+		bazSer := SerializeAndPrint("baz", baz)
+
+		bazDes := Baz{}
+		_ = json.Unmarshal(bazSer, &bazDes)
+		SerializeAndPrint("bazDes", bazDes)
+		fmt.Println()
+	}()
+
+	func() {
+		fuz1 := Fuz{
+			X1:  91,
+			X2:  92,
+			Xyz: foo,
+		}
+		fuz1Ser := SerializeAndPrint("fuz1", fuz1)
+
+		fuz1Des := Fuz{}
+		_ = json.Unmarshal(fuz1Ser, &fuz1Des)
+		SerializeAndPrint("fuz1Des", fuz1Des)
+		fmt.Println()
+	}()
+
+	func() {
+		fuz2 := Fuz{
+			X1:  91,
+			X2:  92,
+			Xyz: foo,
+		}
+		fuz2Ser := SerializeAndPrint("fuz2", fuz2)
+
+		fuz2Des := Fuz{}
+		FuzUnmarshal(fuz2Ser, &fuz2Des)
+		SerializeAndPrint("fuz2Des", fuz2Des)
+		fmt.Println()
+	}()
+
+	func() {
+		fuz3 := Fuz{
+			X1:  91,
+			X2:  92,
+			Xyz: bar,
+		}
+		fuz3Ser := SerializeAndPrint("fuz3", fuz3)
+
+		fuz3Des := Fuz{}
+		FuzUnmarshal(fuz3Ser, &fuz3Des)
+		SerializeAndPrint("fuz3Des", fuz3Des)
+		fmt.Println()
+	}()
+}
