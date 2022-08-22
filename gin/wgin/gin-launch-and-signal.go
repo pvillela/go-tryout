b5822021f781c9d67fa268629678318e0d370e5c
PVillela <pvillela@gmail.com>
2022-08-22 08:52:48 -0400

    Added GinLaunchAndSignal.

---
+++ gin/wgin/gin-launch-and-signal.go
@@ -0,0 +1,79 @@
+/*
+ *  Copyright © 2021 Paulo Villela. All rights reserved.
+ *  Use of this source code is governed by the Apache 2.0 license
+ *  that can be found in the LICENSE file.
+ */
+
+package wgin
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"io"
+	"os"
+	"strings"
+)
+
+// GinLaunchAndSignal launches Gin on a given port in a separate goroutine and returns
+// a channel that signals when the server is ready and a function to be deferred to
+// close the pipe used in the implementation.
+func GinLaunchAndSignal(engine *gin.Engine, port int) (serverReady chan bool, closePipe func()) {
+	// Create memory pipe for tee with stdout
+	pr, pw := io.Pipe()
+
+	// Define tee
+	mw := io.MultiWriter(os.Stdout, pw)
+
+	// String in stdout that signals when server is ready
+	serverReadyStr := "Listening and serving HTTP on"
+
+	// Channel to signal when server is ready
+	serverReady = make(chan bool)
+
+	// Look for string in stdout and send server ready signal
+	go func() {
+		const bufSize = 100
+		bytes := make([]byte, bufSize)
+		sb := strings.Builder{}
+		found := false
+		for {
+			n, _ := pr.Read(bytes)
+			if !found {
+				sb.Write(bytes[:n])
+				if strings.Contains(sb.String(), serverReadyStr) {
+					found = true
+					serverReady <- true
+				}
+			}
+		}
+	}()
+
+	// Get os pipe reader and writer; writes to pipe writer come out pipe reader
+	r, w, _ := os.Pipe()
+
+	// Create channel to control exit; will block until all copies are finished
+	exit := make(chan bool)
+
+	go func() {
+		// copy all reads from os pipe to multiwriter, which writes to stdout and memory pipe
+		_, _ = io.Copy(mw, r)
+		// when r or w is closed copy will finish and true will be sent to channel
+		exit <- true
+	}()
+
+	// Redefine Gin writer to use tee
+	gin.DefaultWriter = w
+
+	go func() {
+		// Listen and serve on 0.0.0.0:port
+		err := engine.Run(fmt.Sprintf(":%v", port))
+		fmt.Println("Server terminated:", err) // this never prints unless there is an error
+	}()
+
+	closePipe = func() {
+		_ = w.Close()
+		<-exit
+	}
+
+	return serverReady, closePipe
+}
---
+++ gin/wgin/json/main.go
@@ -0,0 +1,97 @@
+/*
+ * Copyright © 2021 Paulo Villela. All rights reserved.
+ * Use of this source code is governed by the Apache 2.0 license
+ * that can be found in the LICENSE file.
+ */
+
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/pvillela/go-tryout/gin/wgin"
+	"net/http"
+	"time"
+)
+
+// Binding from JSON
+type Login struct {
+	User     string `form:"user" json:"user" xml:"user"  binding:"required"`
+	Password string `form:"password" json:"password" xml:"password" binding:"-"`
+}
+
+func main() {
+	router := gin.Default()
+
+	// Example for binding JSON ({"user": "manu", "password": "123"})
+	router.POST("/loginJSON", func(c *gin.Context) {
+		var json Login
+		if err := c.BindJSON(&json); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		if json.User != "manu" || json.Password != "123" {
+			c.JSON(http.StatusUnauthorized, gin.H{"status": "unauthorized"})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"status": "you are logged in"})
+	})
+
+	// Example for binding XML (
+	//	<?xml version="1.0" encoding="UTF-8"?>
+	//	<root>
+	//		<user>manu</user>
+	//		<password>123</password>
+	//	</root>)
+	router.POST("/loginXML", func(c *gin.Context) {
+		var xml Login
+		if err := c.ShouldBindXML(&xml); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		if xml.User != "manu" || xml.Password != "123" {
+			c.JSON(http.StatusUnauthorized, gin.H{"status": "unauthorized"})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"status": "you are logged in"})
+	})
+
+	// Example for binding a HTML form (user=manu&password=123)
+	router.POST("/loginForm", func(c *gin.Context) {
+		var form Login
+		// This will infer what binder to use depending on the content-type header.
+		if err := c.ShouldBind(&form); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		if form.User != "manu" || form.Password != "123" {
+			c.JSON(http.StatusUnauthorized, gin.H{"status": "unauthorized"})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"status": "you are logged in"})
+	})
+
+	serverReady, closePipe := wgin.GinLaunchAndSignal(router, 8080)
+	defer closePipe()
+
+	// Wait until server is ready
+	<-serverReady
+	fmt.Println("***** Server is ready")
+
+	// Keep server running for n * delta seconds
+
+	n := 5
+	delta := 5 * time.Second
+	for i := 1; i <= n; i++ {
+		time.Sleep(delta)
+		fmt.Println("Running server:", (time.Duration(i) * delta).Seconds(),
+			"seconds of", (time.Duration(n) * delta).Seconds(), "seconds")
+	}
+	fmt.Println("Exiting")
+}
